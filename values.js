616e4236cf039bab9fdc7c21c04453fe9aecfdd4
Albrektson <Albrektson@users.noreply.github.com>
2019-02-25 21:24:34 +0100

    Axis Labels, Options

--- values.js
+++ values.js
@@ -1,7 +1,7 @@
 var width = 1280,
     height = 800;
 
-var margin = {top: 20, right: 20, bottom: 20, left: 40},
+var margin = {top: 20, right: 20, bottom: 80, left: 60},
     graphWidth = width - margin.left - margin.right,
     graphHeight = height - margin.top - margin.bottom;
 
@@ -21,34 +21,29 @@ var yScale = d3.scaleLinear()
   .range([graphHeight, 0])
 
 var yAxisCall = d3.axisLeft(yScale)
-  .tickSize(5)
-  .tickPadding(2)
 var yAxis = d3.select("#canvas").append("g")
   .attr("class", "y-axis")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
   .call(yAxisCall)
 
 var xAxisCall = d3.axisBottom(xScale)
-    .tickSize(5)
-    .tickPadding(2)
 var xAxis = d3.select("#canvas").append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(" + margin.left + "," + (height - margin.bottom) + ")")
-    .call(xAxisCall)
+  .attr("class", "x-axis")
+  .attr("transform", "translate(" + margin.left + "," + (height - margin.bottom) + ")")
+  .call(xAxisCall)
 
-update(xData = "A170", yData = "A173", wave = 6)
-/*
-var x = false;
-setInterval(function() {
-  if (x) {
-    update(xData = "A170", wave = 6);
-    x = false;
-  } else {
-    update(xData = "A173", wave = 6);
-    x = true;
-  }
-}, 3000);
-*/
+//axis labels
+d3.select("#canvas").append("text")
+  .attr("class", "x-axis-title")
+  .attr("transform", "translate(" + graphWidth/2 + "," + (height-40) +")")
+  .text("X AXIS")
+d3.select("#canvas").append("text")
+  .attr("class", "y-axis-title")
+  .attr("transform", "translate(" + 20 + "," + (graphHeight/2) +"), rotate(-90)")
+  .text("Y AXIS")
+
+update(xData = "A170", yData = "A173", wave = 5)
+
 function update(xData, yData, wave) {
   d3.csv("stats.csv").then(function(stats){
     var bubbles = d3.select("#graph")
@@ -69,10 +64,19 @@ function update(xData, yData, wave) {
 
     //update values
     bubbles
-      .attr("cx", function(d, i){
-        //return xScale(i)
-        return xScale(+d[xData]);
-      })
+    .attr("cx", function(d, i){
+      return xScale(+d[xData]);
+    })
+    .attr("cy", function(d, i) {
+      return yScale(+d[yData]);
+    })
+    .attr("r", function(d){
+      if (+d["Wave"] != wave || d[xData] == "NA" || d[yData] == "NA") {
+          return 0;
+      } else {
+        return 3;
+      }
+    })
 
     bubbles.enter().append("circle")
       .attr("cx", function(d, i){
@@ -82,10 +86,10 @@ function update(xData, yData, wave) {
         return yScale(+d[yData]);
       })
       .attr("r", function(d){
-        if (+d["Wave"] == wave) {
-            return 3;
+        if (+d["Wave"] != wave || d[xData] == "NA" || d[yData] == "NA") {
+            return 0;
         } else {
-          return 0;
+          return 3;
         }
       })
       .attr("fill", "grey")
@@ -95,10 +99,27 @@ function update(xData, yData, wave) {
         return d["Country"]+d["Wave"]
       });
 
-      bubbles.exit().remove()
+    bubbles.exit().remove()
 
-      //update and scale axes
-      xAxis.call(xAxisCall)
-      yAxis.call(yAxisCall)
+    //update and scale axes
+    xAxis.call(xAxisCall)
+    yAxis.call(yAxisCall)
+
+    d3.select('#xOpts').on('change', function() {
+      var input = eval(d3.select(this).property('value'));
+      return update(input, yData, wave)
+    })
+    d3.select('#yOpts').on('change', function() {
+      var input = eval(d3.select(this).property('value'));
+      return update(xData, input, wave)
+    })
+    d3.select('#sizeOpts').on('change', function() {
+      var input = eval(d3.select(this).property('value'));
+      return update(xData, yData, wave)
+    })
+    d3.select('#waveOpts').on('change', function() {
+      var input = eval(d3.select(this).property('value'));
+      return update(xData, yData, input)
+    })
   })
 }
